ad177434b0b7e5607341cc1157716d8c6dee4a40
Anurag7275977 <arjunsharma705463@gmail.com>
2023-04-27 01:02:57 +0530

    Fifth Assignmnet complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Counterapp.css'; // import CSS file
 
-function App() {
+ function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  }
+
+  const decreaseCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+      <h1 className="count">Count: {count}</h1>
+      <div className="button-container">
+        <button className="increase-button" onClick={increaseCount}>Increase</button>
+        <button className="decrease-button" onClick={decreaseCount}>Decrease</button>
+      </div>
     </div>
   );
 }
